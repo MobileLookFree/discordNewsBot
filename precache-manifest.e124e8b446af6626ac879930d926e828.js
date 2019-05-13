82483e0a8ec86c3472e25fb90e022e3ae00ff8be
Andrei <mobilelookfree@gmail.com>
2019-05-13 18:15:34 +0300

    Updates

---
+++ precache-manifest.e124e8b446af6626ac879930d926e828.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "44481877f7c366c999e9",
+    "url": "/discordNewsBot/static/js/runtime~main.44481877.js"
+  },
+  {
+    "revision": "f5ae7dbd238dfdf66bd5",
+    "url": "/discordNewsBot/static/js/main.f5ae7dbd.chunk.js"
+  },
+  {
+    "revision": "3f0b22993c54ddcf39f8",
+    "url": "/discordNewsBot/static/js/1.3f0b2299.chunk.js"
+  },
+  {
+    "revision": "f5ae7dbd238dfdf66bd5",
+    "url": "/discordNewsBot/static/css/main.cb256e78.chunk.css"
+  },
+  {
+    "revision": "6b3c2effaa434c862f9938c039ab2c7b",
+    "url": "/discordNewsBot/index.html"
+  }
+];
